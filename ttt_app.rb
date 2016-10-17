a87654c85c765f84f651edfd76aa61010bc3215d
John Erickson <ericksonjohn08@hotmail.com>
2016-10-17 11:39:03 -0400

    Added human vs human code.

--- ttt_app.rb
+++ ttt_app.rb
@@ -18,32 +18,49 @@ get '/' do
 end
 
 post '/game' do
-	player_1 = params[player_1]
-	player_2 = params[player_2]
-
-	if player_1 == "human"
-		session[:p1] = Human.new("X")
-	elsif player_1 == "sequential_ai"
-		session[:p1] = SequentialAI.new("X")
-	elsif player_1 == "random_ai"
-		session[:p1] = RandomAI.new("X")
-	else player_1 == "unbeatable_ai"
-		session[:p1] = UnbeatableAI.new("X")
-	end	
-
-	if player_2 == "human"
-		session[:p2] = Human.new("O")
-	elsif player_2 == "sequential_ai"
-		session[:p2] = SequentialAI.new("O")
-	elsif player_2 == "random_ai"
-		session[:p2] = RandomAI.new("O")
-	else player_2 == "unbeatable_ai"
-		session[:p2] = UnbeatableAI.new("O")
-	end
+	session[:name_player_1] = params[:player_1]
+	# player_2 = params[player_2]
+	session[:p1] = Human.new("X")
+	
+	# Can remove if post '/opponent' functions correctly
+	# if player_2 == "human"
+	# 	session[:p2] = Human.new("O")
+	# elsif player_2 == "sequential_ai"
+	# 	session[:p2] = SequentialAI.new("O")
+	# elsif player_2 == "random_ai"
+	# 	session[:p2] = RandomAI.new("O")
+	# else player_2 == "unbeatable_ai"
+	# 	session[:p2] = UnbeatableAI.new("O")
+	# end
 
 	session[:current_player] = session[:p1]
+	session[:current_player_name] = session[:name_player_1]
 
-	erb :get_move, :locals => { :current_player => session[:current_player], :p1 => session[:p1], :p2 => session[:p2], :board => session[:board].board_positions }		
+	erb :opponent, :locals => { :current_player => session[:current_player], :p1 => session[:p1], :player_1_name => session[:name_player_1], :current_player_name => session[:current_player_name], :board => session[:board].board_positions }		
+end
+
+post '/opponent' do
+	player_2 = params[:player_2]
+	if player_2 == "human"
+		session[:p2] = Human.new("O")
+
+		erb :opponent_name, :locals => { :current_player => session[:current_player], :p1 => session[:p1], :player_1_name => session[:name_player_1], :p2 => session[:p2], :current_player_name => session[:current_player_name], :board => session[:board].board_positions }
+	elsif player_2 == "sequential_ai"
+		session[:p2] = SequentialAI.new("O")
+		erb :get_move, :locals => { :current_player => session[:current_player], :p1 => session[:p1], :player_1_name => session[:name_player_1], :p2 => session[:p2], :current_player_name => session[:current_player_name], :board => session[:board].board_positions }	
+	elsif player_2 == "random_ai"
+		session[:p2] = RandomAI.new("O")
+		erb :get_move, :locals => { :current_player => session[:current_player], :p1 => session[:p1], :player_1_name => session[:name_player_1], :p2 => session[:p2], :current_player_name => session[:current_player_name], :board => session[:board].board_positions }
+	else player_2 == "unbeatable_ai"
+		session[:p2] = UnbeatableAI.new("O")
+		erb :get_move, :locals => { :current_player => session[:current_player], :p1 => session[:p1], :player_1_name => session[:name_player_1], :p2 => session[:p2], :current_player_name => session[:current_player_name], :board => session[:board].board_positions }
+	end
+end
+
+post '/opponent_name' do
+	session[:p2_name] = params[:player_2]
+
+	erb :get_move, :locals => { :current_player => session[:current_player], :p1 => session[:p1], :player_1_name => session[:name_player_1], :player_2_name => session[:p2_name], :p2 => session[:p2], :current_player_name => session[:current_player_name], :board => session[:board].board_positions }
 end
 
 get '/get_move' do
@@ -56,17 +73,19 @@ post '/get_player_move' do
 	session[:board].update_board((move_spot - 1), session[:current_player].marker)
 
 	if session[:board].game_won?(session[:current_player].marker) == true
-		erb :win, :locals => { :current_player => session[:current_player], :p1 => session[:p1], :p2 => session[:p2], :board => session[:board].board_positions }
+		erb :win, :locals => { :current_player => session[:current_player], :p1 => session[:p1], :p2 => session[:p2], :current_player_name => session[:current_player_name], :board => session[:board].board_positions }
 	elsif session[:board].game_ends_in_tie? == true
-		erb :tie, :locals => { :current_player => session[:current_player], :p1 => session[:p1], :p2 => session[:p2], :board => session[:board].board_positions }
+		erb :tie, :locals => { :current_player => session[:current_player], :p1 => session[:p1], :p2 => session[:p2], :current_player_name => session[:current_player_name], :board => session[:board].board_positions }
 	else
 		if session[:current_player].marker == "X"
 			session[:current_player] = session[:p2]
+			session[:current_player_name] = session[:p2_name]
 		else
 			session[:current_player] = session[:p1]
+			session[:current_player_name] = session[:name_player_1]
 		end
 
-		erb :get_move, :locals => { :current_player => session[:current_player], :p1 => session[:p1], :p2 => session[:p2], :board => session[:board].board_positions }
+		erb :get_move, :locals => { :current_player => session[:current_player], :p1 => session[:p1], :player_1_name => session[:name_player_1], :player_2_name => session[:p2_name], :p2 => session[:p2], :current_player_name => session[:current_player_name], :board => session[:board].board_positions }
 
 	end	
 end
