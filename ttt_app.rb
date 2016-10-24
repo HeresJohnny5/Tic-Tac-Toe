2c015cbf6ff3ed8a9c8008428e900e3907ea79d7
John Erickson <ericksonjohn08@hotmail.com>
2016-10-24 13:47:41 -0400

    Working on connecting csv to bucket.

--- ttt_app.rb
+++ ttt_app.rb
@@ -7,21 +7,29 @@ require_relative 'random_ai.rb'
 require_relative 'unbeatable_ai.rb'
 require 'aws/s3' 
 require 'sinatra/reloader' if development?
-# load './local_env.rb' if File.exists?("./local_env.rb")
+load './local_env.rb' if File.exists?("./local_env.rb")
 
-def write_file_to_s3(data_to_write)
+def write_file_to_s3(player_1, player_2, winner, date_time)
 	AWS::S3::Base.establish_connection!(
-  :access_key_id => 'AKIAIP2WO3P4LTNQ42UQ',
-  :secret_access_key => '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'   
+  :access_key_id => ENV['S3_KEY'],
+  :secret_access_key => ENV['S3_SECRET']   
 )
 	file = 'summary.csv' 
-	bucket = 'tictactoe-scores5'
+	bucket = 'tictactoe-scores1'
 	csv = AWS::S3::S3Object.value(file, bucket)
-	csv << "Something"
+	csv << player_1 + ", " + player_2 + ", " + winner + ", " + date_time.to_s + "\n"
 	AWS::S3::S3Object.store(File.basename(file), 
         csv, 
         bucket, 
         :access => :public_read)	
+end
+
+# Added method 28
+def read_csv_from_s3
+	file = ENV['S3_File']
+	bucket = ENV['S3_BUCKET']
+	object_from_s3 = AWS::S3::S3Object.value(file, bucket)
+	csv = CSV.parse(object_from_s3)
 end
 
 enable :sessions
@@ -110,7 +118,7 @@ get '/make_move' do
 		write_to_csv(player_1, player_2, winner, date_time)
 
 		# Added line 134
-		write_file_to_s3(player_1)
+		write_file_to_s3(player_1, player_2, winner, date_time)
 
 		erb :win, :locals => { :current_player => session[:current_player], :current_player_name => session[:current_player_name], :board => session[:board].board_positions }
 	elsif session[:board].game_ends_in_tie? == true
